3dca0e83e112fbdc6d146f967dc44cc7c3a6b059
Unknown <up201403027@fe.up.pt>
2018-04-12 10:08:58 +0100

    [docker-image] update credentials on upload image to docker hub
    must docker login before

--- lbaw-laravel/upload_image.sh
+++ lbaw-laravel/upload_image.sh
@@ -3,8 +3,8 @@
 # Stop execution if a step fails
 set -e
 
-DOCKER_USERNAME=tiagoboldt # Replace by your docker hub username
-IMAGE_NAME=lbaw-demo
+DOCKER_USERNAME=lbaw1721 # Replace by your docker hub username
+IMAGE_NAME=lbaw1721
 
 # Ensure that dependencies are available
 composer install
