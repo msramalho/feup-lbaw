5745a02ddddbcfb308ab619bf5e58e57dad316b7
Unknown <up201403027@fe.up.pt>
2018-04-15 18:30:43 +0100

    [blade] footer

---
+++ lbaw-laravel/resources/views/partials/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="container py-5 p-3">
+	<div class="row">
+		<div class="text-center col-sm col-md">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+				stroke-linecap="round" stroke-linejoin="round">
+				<line x1="1" y1="1" x2="12" y2="23"></line>
+				<line x1="12" y1="23" x2="23" y2="1"></line>
+			</svg>
+			<small style="vertical-align:super;" class="mb-3 text-muted">&copy; 2018 Made with &#x2764; @FEUP</small>
+		</div>
+		<div class="text-center col-sm col-md">
+			<ul class="list-inline text-small">
+				<li class="list-inline-item">
+					<a class="text-muted" href="{{ url('/faq') }}">FAQ</a>
+				</li>
+				<li class="list-inline-item">
+					<a class="text-muted" href="{{ url('/about') }}">About Vecto</a>
+				</li>
+				<li class="list-inline-item">
+					<a class="text-muted" href="{{ url('/contacts') }}">Contacts</a>
+				</li>
+				<li class="list-inline-item">
+					<a class="text-muted" href="{{ url('/statistics') }}">Statistics</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</footer>
