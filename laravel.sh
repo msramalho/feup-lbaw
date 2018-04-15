d29a88f45df7f9750f225d5e441d334854be6760
Unknown <up201403027@fe.up.pt>
2018-04-15 17:54:03 +0100

    bash file to simplify start up and shut down
    just do 
    sh laravel.sh [up|down]

---
+++ laravel.sh
@@ -0,0 +1,17 @@
+cd lbaw-laravel
+
+
+# Do `sh laravel.sh up`   to start app
+# Do `sh laravel.sh down` to stop  app
+
+if [ $1 = "up" ]; then
+	docker-compose up -d
+	php artisan serve &
+	php artisan up
+	xdg-open "http://127.0.0.1:8000/"
+elif [ $1 = "down" ]; then
+	docker-compose down
+	php artisan down
+	# kill $(ps aux | grep artisan | awk '{print $2}')
+	kill $(lsof -t -i:8000)
+fi
