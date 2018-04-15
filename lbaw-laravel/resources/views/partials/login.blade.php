52b92bb437b8d5f33ebb422769581f9a6c507e0a
Unknown <up201403027@fe.up.pt>
2018-04-15 18:24:28 +0100

    [blad] navbar pages

---
+++ lbaw-laravel/resources/views/partials/login.blade.php
@@ -0,0 +1,21 @@
+<li class="nav-item">
+	<div class="dropdown">
+		<a class="nav-link dropdown-toggle" href="#" id="logInDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Log In
+		</a>
+		<div class="dropdown-menu dropdown-menu-right">
+			<form class="px-4 py-3" action="index-admin.html" method="get">
+				<div class="form-group">
+					<input type="text" class="form-control" id="login_username_email" name="login_username_email" placeholder="username or email"
+					required>
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" id="login_pass" name="login_pass" placeholder="password" required>
+				</div>
+				<button type="submit" class="btn btn-dark">Log In</button>
+			</form>
+			<div class="dropdown-divider"></div>
+			<a class="dropdown-item" href="{{ url('/recover-password') }}">Forgot password? No problem</a>
+		</div>
+	</div>
+</li>
---
+++ lbaw-laravel/resources/views/partials/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-navbar">
+	<div class="container ">
+		<a class="navbar-brand" href="{{ url('/') }}">Vecto</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
+			aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav mr-auto"></ul>
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<form class="form-inline mr-4">
+						<input class="form-control mr-sm-2" type="search" placeholder="Search">
+						<button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+					</form>
+				</li>
+
+				@if (Auth::check())
+					@include('partials.logout')
+				@else
+					@include('partials.register')
+					@include('partials.login')
+				@endif
+			</ul>
+		</div>
+	</div>
+</nav>
