a4bdf6d2003daa905dd2934749af135e82e5ef89
Daniel Silva <danny@dannyps.net>
2018-04-16 14:35:49 +0100

    [controller] fix data password

--- lbaw-laravel/app/Http/Controllers/Auth/RegisterController.php
+++ lbaw-laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $date["password"] = bcrypt($data['password']);
+        $data["password"] = bcrypt($data['password']);
         return User::create($data);
     }
 }
