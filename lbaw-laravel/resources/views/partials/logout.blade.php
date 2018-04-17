ef176116347dbc8e430edaf39b480eb381b6460d
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-04-17 18:56:58 +0100

    Added correct login dropdown

--- lbaw-laravel/resources/views/partials/logout.blade.php
+++ lbaw-laravel/resources/views/partials/logout.blade.php
@@ -1,1 +1,14 @@
-<a href="{{ url("logout") }}" type="submit" class="btn">Logout</a>
+<li class="nav-item">
+    <div class="dropdown">
+        <div class="dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="user_dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user-circle"></i> lorenu</a>
+            <div class="dropdown-menu" aria-labelledby="user_dropdown">
+                <a class="dropdown-item" href="edit-profile.html">
+                    <i class="fas fa-cog"></i> Edit Profile</a>
+                <a class="dropdown-item" href="{{ url("logout") }}">
+                    <i class="fas fa-sign-out-alt"></i> Logout</a>
+            </div>
+        </div>
+    </div>
+</li>
