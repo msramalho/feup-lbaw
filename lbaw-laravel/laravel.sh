607445a36cafcb91c669fa200184e3c171440594
msramalho <up201403027@fe.up.pt>
2018-04-17 18:36:37 +0100

    moved bash file to inner folder

--- lbaw-laravel/laravel.sh
+++ lbaw-laravel/laravel.sh
@@ -1,6 +1,3 @@
-cd lbaw-laravel
-
-
 # Do `sh laravel.sh up`   to start app
 # Do `sh laravel.sh down` to stop  app
 
@@ -12,7 +9,6 @@ if [ $1 = "up" ]; then
 	xdg-open "http://127.0.0.1:5050/"
 elif [ $1 = "down" ]; then
 	docker-compose down
-	php artisan down
 	# kill $(ps aux | grep artisan | awk '{print $2}')
 	kill $(lsof -t -i:8000)
 fi
