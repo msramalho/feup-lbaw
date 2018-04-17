08117127b8e893b474995e00187c8a61e55da361
msramalho <up201403027@fe.up.pt>
2018-04-17 20:01:52 +0100

    [ignore] removed include

--- lbaw-laravel/app/User.php
+++ lbaw-laravel/app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\CanResetPassword;
+
 
 class User extends Authenticatable
 {
