1caaaddd36fbfb3f654d8652ba904042f81dba31
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-04-17 18:59:38 +0100

    Now with correct username

--- lbaw-laravel/resources/views/partials/logout.blade.php
+++ lbaw-laravel/resources/views/partials/logout.blade.php
@@ -2,7 +2,7 @@
     <div class="dropdown">
         <div class="dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="user_dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-user-circle"></i> lorenu</a>
+                <i class="fas fa-user-circle"></i> {{$name = \Auth::user()->name}}</a>
             <div class="dropdown-menu" aria-labelledby="user_dropdown">
                 <a class="dropdown-item" href="edit-profile.html">
                     <i class="fas fa-cog"></i> Edit Profile</a>
