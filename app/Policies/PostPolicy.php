3e21d7738633d6b964116c4ffc3e396266087178
msramalho <up201403027@fe.up.pt>
2018-04-22 12:42:03 +0100

    [policy] for post

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Post;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Auth;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function view(User $user, Post $post)
+    {
+        return true; // anyone can view a post
+    }
+
+    /**
+     * Determine whether the user can create posts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function update(User $user, Post $post)
+    {
+        return $user->id === $post->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        return $user->id === $post->user_id;
+    }
+}
