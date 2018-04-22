7e7efb1fa8e39390587526d6872fe9be5945a183
msramalho <up201403027@fe.up.pt>
2018-04-22 17:14:37 +0100

    [controller] post - only creation

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $this->authorize('create', Post::class);
+        $this->validate($request, [
+            'from_faculty_id' => 'required|numeric',
+            'to_faculty_id' => 'required|numeric',
+            'title' => 'required|string|max:255',
+            'content' => 'required|string|max:5000',
+            'school_year' => 'required|numeric',
+            'beer_cost' => 'required|string',
+            'life_cost' => 'required|string',
+            'native_friendliness' => 'required|string',
+            'work_load' => 'required|string',
+        ], ["from_faculty_id.*"=>"The origin faculty must be set", "to_faculty_id.*" => "the destination faculty must be set"]);
+        $post = new Post($request->all());
+        $post->author_id = Auth::user()->id;
+        $post->save();
+        return Redirect::to("post/$post->id");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+
+
+
+
+    /**
+     * Get a validator for an incoming post creation request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        //'name', 'email', 'password', 'birthdate', 'name', 'register_date', 'description', 'last_login', 'type',
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'username' => ['required','string','regex:/^[^@]*$/', 'unique:users'],
+            'password' => 'required|string|min:6', 
+            'birthdate' => 'date|before:today',
+            'description' => 'string|max:5000',
+        ], ["username.regex" => "username cannot have a @ char"]);
+    }
+}
