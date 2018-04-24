9b636f24c0a882a2c09bad126ac930f2feece15d
Daniel Silva <danny@dannyps.net>
2018-04-24 21:40:21 +0100

    [view] show (no) comment(s)/() correctly.

--- resources/views/pages/post/show.blade.php
+++ resources/views/pages/post/show.blade.php
@@ -63,23 +63,34 @@
 				</div>
 				<hr>
 				<div class="article-comments">
+					@php ($cmCount = count($post->comments()->get()))
+					@if($cmCount>1)
 					<h2>
-						{{ count($post->comments()->get()) }} comments
+						{{ $cmCount }} comments:
 					</h2>
+					@elseif($cmCount==1)
+					<h2>
+						1 comment:
+					</h2>
+					@else
+					<h2>
+						No comments yet.
+					</h2>
+					@endif
 					@foreach ($post->comments()->get() as $cm)
 						<div id="c{{ $cm->id }}" class="p-3 bg-light article-comment">
 							<img src="{{ asset('/images/profile.png') }}" alt="Profile Picture">
-							<h3>{{ $cm->user()->get()[0]->username }}</h3>
+							<h3>{{ $cm->user->username }}</h3>
 							<p>{{ $cm->content }}</p>
 							<div class="flag-comment small text-secondary">
 								<a class="text-secondary" href="#">
 									<i class="fas fa-flag"></i>
-									<span>&nbsp; Flag this comment</span>
+									<span>Flag</span>
 								</a>
 								@if($cm->isOwner())
 									<a class="text-secondary" href="#">
 										<i class="fas fa-trash"></i>
-										<span>Delete this comment</span>
+										<span>Delete</span>
 									</a>
 								@endif
 							</div>
