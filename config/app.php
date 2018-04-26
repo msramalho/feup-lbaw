d81c034ad35901c6a056963b5be6dec06e76c450
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-04-26 10:49:58 +0100

    Intervention Image Manager

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,6 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Post' => App\Http\Controllers\PostController::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
