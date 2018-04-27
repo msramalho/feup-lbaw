3abaf51497a633cd12d7ec5b720dc07c52ccdec3
msramalho <up201403027@fe.up.pt>
2018-04-27 21:15:19 +0100

    [blade] navbar now shows link to personal profile

--- resources/views/partials/account.blade.php
+++ resources/views/partials/account.blade.php
@@ -2,8 +2,10 @@
     <div class="dropdown">
         <div class="dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="user_dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-user-circle"></i> {{$username = \Auth::user()->username}}</a>
+                <i class="fas fa-user-circle"></i> {{$username = Auth::user()->username}}</a>
             <div class="dropdown-menu" aria-labelledby="user_dropdown">
+                <a class="dropdown-item" href="{{ url('user/' . Auth::user()->username) }}">
+                    <i class="fas fa-user"></i> {{Auth::user()->username}}</a>
                 <a class="dropdown-item" href="{{ url("user/edit") }}">
                     <i class="fas fa-cog"></i> Edit Profile</a>
                 <a class="dropdown-item" href="{{ url("logout") }}">
