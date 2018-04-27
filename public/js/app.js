7102b87ed4f00bcc933b0c121091699f184d478c
msramalho <up201403027@fe.up.pt>
2018-04-27 21:11:27 +0100

    [javascript] app.js with global functions

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+function copyToClipboard(text){
+	var textArea = document.createElement("textarea");
+	textArea.value = text;
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
+	
+	/* Copy the text inside the text field */
+	document.execCommand("Copy");
+	
+	alert("text coppied successfully");
+
+	document.body.removeChild(textArea);
+}
+
+(function(proxied) {
+	window.alert = function(text) {
+		text = "TODO in app.js...\n"+text;
+		return proxied.apply(this, arguments);
+	};
+})(window.alert);
