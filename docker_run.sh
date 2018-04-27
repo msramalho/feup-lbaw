9fb7b106802c720cb4e2206ef1ba48f1bfe575bc
msramalho <up201403027@fe.up.pt>
2018-04-27 13:43:37 +0100

    [DOCKER] PRODUCTION IS FIXED!!!!!!

--- docker_run.sh
+++ docker_run.sh
@@ -3,6 +3,11 @@ set -e
 
 env >> /var/www/.env
 php-fpm7.1 -D
+
 cd /var/www
-php artisan migrate
+php artisan migrate --force
+# php /var/www/artisan config:clear
+# php /var/www/artisan db:seed
+
+# env DB_HOST=127.0.0.1 php artisan migrate
 nginx -g "daemon off;"
