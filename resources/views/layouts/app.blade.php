c3f08b848e198caf3cca56d87cf3bbb1e9d25d42
Daniel Silva <danny@dannyps.net>
2018-05-03 10:45:51 +0100

    remove defer from srcless script tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
 <script src="{{ asset('js/external/bootstrap.min.js') }}" ></script>
 <script src="{{ asset('js/external/swal.min.js') }}" ></script>
 <script src="{{ asset('js/app.js') }}" ></script>
-<script defer>
+<script>
     $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
 </script>
 @yield('scripts')
