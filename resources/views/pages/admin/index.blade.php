0e47ee8b9b14b111828adb0d4e24608083bba352
msramalho <up201403027@fe.up.pt>
2018-05-03 11:08:23 +0100

    admin page

---
+++ resources/views/pages/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Vecto: Admin Dashboard')
+
+@section('content')
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Admin Dashboard</h1>
+			<p class="lead">Manage them. Manage them all.</p>
+			<a href="{{ url('admin/universities') }}" class="btn btn-lg btn-primary">Manage Universities</a>
+			<a href="{{ url('admin/users') }}" class="btn btn-lg btn-primary">Manage Users</a>
+			<a href="{{ url('admin/flags') }}" class="btn btn-lg btn-primary">Manage Flags</a>
+		</div>
+	</div>	
+@endsection
