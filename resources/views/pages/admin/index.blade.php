1b60ee0d4e6fe513048650309793601e93975d1e
msramalho <up201403027@fe.up.pt>
2018-05-03 21:26:29 +0100

    link for city management in admin dashboard

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -10,6 +10,7 @@
 			<a href="{{ url('admin/universities') }}" class="btn btn-lg btn-primary">Manage Universities</a>
 			<a href="{{ url('admin/users') }}" class="btn btn-lg btn-primary">Manage Users</a>
 			<a href="{{ url('admin/flags') }}" class="btn btn-lg btn-primary">Manage Flags</a>
+			<a href="{{ url('admin/cities') }}" class="btn btn-lg btn-primary">Manage Cities</a>
 		</div>
 	</div>	
 @endsection
