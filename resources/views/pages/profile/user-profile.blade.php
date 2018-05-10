c198a4aff7d2196d98caba3834ea29cc0a41009b
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-10 13:37:21 +0100

    [html] user votes

--- resources/views/pages/profile/user-profile.blade.php
+++ resources/views/pages/profile/user-profile.blade.php
@@ -133,7 +133,7 @@
 
                     <section class="jqueryOptions option4 d-none">
                         <div class="feed-content">
-                            @each('pages.post.list_item', Post::getIndexList(), 'post')
+                            @each('pages.post.list_item', Post::view_posts_votes($user->id), 'post')
                         </div>
                     </section>
                 </div>
