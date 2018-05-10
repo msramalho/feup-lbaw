49f2795350e103d83bbf2932d7e6c1e8ac4b2f08
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-10 11:40:41 +0100

    [html] User posts

--- resources/views/pages/profile/user-profile.blade.php
+++ resources/views/pages/profile/user-profile.blade.php
@@ -121,13 +121,13 @@
 
                     <section class="jqueryOptions option2 d-none">
                         <div class="feed-content">
-                            {{-- @each('pages.post.list_item', Post::view_posts($user->id), 'post') Uncomment for testing--}}
+                            @each('pages.post.list_item', Post::view_posts($user->id), 'post')
                         </div>
                     </section>
 
                     <section class="jqueryOptions option3 d-none">
                         <div class="feed-content">
-                            @each('pages.post.list_item', Post::getIndexList(), 'post')
+                            @each('pages.post.list_item', Post::view_posts($user->id), 'post')
                         </div>
                     </section>
 
