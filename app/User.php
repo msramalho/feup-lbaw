82e130aeb263b4933ebba11be33afe581ce7119a
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-13 01:52:45 +0100

    Repeated name

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'birthdate', 'name', 'description', 'username',
+        'name', 'email', 'password', 'birthdate', 'description', 'username',
     ];
 
     /**
