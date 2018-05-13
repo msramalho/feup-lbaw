e1e1749310fec15ecdbd331b10c5b3ded56f81aa
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-13 03:32:02 +0100

    Index With Options

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,10 +11,25 @@
 		</div>
 		
 		@if(Auth::check())
-			<h3>Friends Recent Posts:</h3>
-			<div id="feed-content">
-				@each('pages.post.list-item', Post::getFollowersList(Auth::user()->id), 'post')
+			<div class="btn-group btn-group-toggle pb-3" data-toggle="buttons" id="view-index-options">
+				<label class="btn btn-secondary active">
+					<input type="radio" name="options" value="option1" checked > Friends Recent Posts </input>
+				</label>
+				<label class="btn btn-secondary">
+					<input type="radio" name="options" value="option2"> Recent Posts </input>
+				</label>
 			</div>
+			<section class="jqueryOptions option1">
+				<div id="feed-content">
+					@each('pages.post.list-item', Post::getFollowersList(Auth::user()->id), 'post')
+				</div>
+			</section>
+
+			<section class="jqueryOptions option2 d-none">
+				<div id="feed-content">
+					@each('pages.post.list-item', Post::getIndexList(), 'post')
+				</div>
+			</section>
 		@else
 			<h3>Recent Posts:</h3>
 			<div id="feed-content">
@@ -27,6 +42,6 @@
 		</div>
 	</div>
 
-	<script type="text/javascript" src="{{ asset('js/pages/html-elements.js') }}" defer></script>
+	<script type="text/javascript" src="{{ asset('js/pages/index.js') }}" defer></script>
 
 @endsection
