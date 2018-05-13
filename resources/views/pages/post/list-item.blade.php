05304d00595a0dfc101e1e97edde2050335a2983
Afonso Jorge Ramos <up201506239@fe.up.pt>
2018-05-13 18:39:13 +0100

    Greatly Improved mobile usage

--- resources/views/pages/post/list-item.blade.php
+++ resources/views/pages/post/list-item.blade.php
@@ -2,7 +2,8 @@
     <div class="row">
         <div class="text-center col-xs-12 col-lg-1 col-xl-1 col-sm-12 col-md-1">
             <p class="vote-count-index">
-                {{$post->votes}}
+                {{$post->votes}}<br>
+                <a href="{{ url('/post/'.$post->id) }}"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
             </p>
         </div>
         <div class="col-lg-11 col-sm-12 col-xl-11 col-md-11">
@@ -12,17 +13,17 @@
             </a>
             <p id="short">{{str_limit($post->content, 300)}}</p>
             <div class="row">
-                <div class="text-center col-lg-2 col-3">
+                <div class="text-center col-lg-2 col-md-4 col-sm-5 col-4">
                     <i class="fas fa-user-circle"></i> <a href="{{ url('/user/'.$post->user->username) }}">{{$post->user->username}}</a>
                 </div>
-                <div class="text-center col-lg-6 col-4">
+                <div class="text-center col-lg-6 col-md-4 col-sm-5 col-5">
                     <i class="fas fa-map-marker-alt"></i> <a href="{{ url('/search/faculty/'.$post->faculty_to->id.'') }}">{{$post->faculty_to->name}}</a>
                 </div>
-                <div class="text-center col-lg-2 col-2">
+                <div class="text-center col-lg-2col-4 d-none d-md-block">
                     <i class="fas fa-calendar-alt"></i> <a href="search.html?year={{$post->school_year}}">{{$post->school_year}}/{{$post->school_year+1}}</a> 
                 </div>
-                <div class="text-center col-lg-2 col-2">
-                    <i class="fas fa-comments"></i><a href="{{ url('/post/'.$post->id) }}"> {{count($post->comments)}} comment(s)</a>
+                <div class="text-center col-lg-2 col-md-2 col-sm-2 col-3">
+                    <i class="fas fa-comments"></i><a href="{{ url('/post/'.$post->id) }}"> {{count($post->comments)}}</a>
                 </div>
             </div>
         </div>
