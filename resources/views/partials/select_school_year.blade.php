5af5adf164474f6781063a3f8fc5a56ed94c4fc9
msramalho <up201403027@fe.up.pt>
2018-05-22 23:53:16 +0100

    [blade] updated data select include

--- resources/views/partials/select_school_year.blade.php
+++ resources/views/partials/select_school_year.blade.php
@@ -1,6 +1,6 @@
 <select class="custom-select" name="school_year" required>
 	<option selected>During</option>
-	@for ($i = 15; $i < substr(date("Y"), 2); $i++)
-		<option value="{{$i}}" {{ $i == old("school_year")?"selected":""}}>{{$i}}/{{$i+1}}</option>
+	@for ($i = 10; $i < substr(date("Y"), 2); $i++)
+		<option value="{{$i}}" {{ $i == old("school_year", Request::get('school_year'))?"selected":""}}>{{$i}}/{{$i+1}}</option>
 	@endfor
 </select>
