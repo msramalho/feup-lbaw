23ce50aa2e027f2b4ddc8a5f497af70b4b971d6b
msramalho <up201403027@fe.up.pt>
2018-05-23 00:32:47 +0100

    [blade] select school_year valid default

--- resources/views/partials/select_school_year.blade.php
+++ resources/views/partials/select_school_year.blade.php
@@ -1,5 +1,5 @@
 <select class="custom-select" name="school_year" required>
-	<option selected>During</option>
+	<option value="-1" selected>During</option>
 	@for ($i = 10; $i < substr(date("Y"), 2); $i++)
 		<option value="{{$i}}" {{ $i == old("school_year", Request::get('school_year'))?"selected":""}}>{{$i}}/{{$i+1}}</option>
 	@endfor
