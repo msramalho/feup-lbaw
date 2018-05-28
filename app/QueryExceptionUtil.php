6d971a187d23e6ba9d1b1e2856923b4b1621a9a7
msramalho <up201403027@fe.up.pt>
2018-05-28 19:48:03 +0100

    QueryExceptionUtil return default if not query exception

--- app/QueryExceptionUtil.php
+++ app/QueryExceptionUtil.php
@@ -5,6 +5,9 @@ namespace App;
 class QueryExceptionUtil
 {
 	public static function getErrorFromException($exception){
-		return "DB:" . explode(": ", explode("\n", $exception->errorInfo[2])[0])[1];
+		if (is_a($exception, "Illuminate\Database\QueryException")) {
+			return "DB:" . explode(": ", explode("\n", $exception->errorInfo[2])[0])[1];
+		}
+		return $exception->getMessage();
 	}
 }
