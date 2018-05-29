541ccafd6d36323b2c9398df42dab44b84fbc24f
msramalho <up201403027@fe.up.pt>
2018-05-29 12:27:04 +0100

    include in edit post

--- resources/views/pages/post/edit.blade.php
+++ resources/views/pages/post/edit.blade.php
@@ -39,9 +39,7 @@
 					<label>School Year</label>
 					<select class="custom-select" name="school_year" required>
 						<option selected>During</option>
-						@for ($i = 15; $i < substr(date("Y"), 2); $i++)
-							<option value="{{$i}}" {{ $i == (old("school_year")??$post->school_year)?"selected":""}}>{{$i}}/{{$i+1}}</option>
-						@endfor
+						@include("partials.select_school_year")
 					</select>
 				</div>
 			</div>
