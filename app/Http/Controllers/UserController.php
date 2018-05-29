154ca02a5073ed14152e6fe232ebc8c603de57bd
Daniel Silva <danny@dannyps.net>
2018-05-29 15:27:23 +0100

    closes #52

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Comment;
 use App\Upload;
 use App\Following;
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
